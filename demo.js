fcc078a7d09c8cdb13f4bf342145b86568a26af0
Ayaj Mulla <ayajmulla2341@gmail.com>
2026-04-08 11:00:27 +0530

    basic json

---
+++ demo.js
@@ -0,0 +1,22 @@
+// demo json
+const json = {
+    "name": "ayaj",
+    "surname": "mulla",
+    "description": "null",
+    "age": 22,
+    "gender": "male",
+    "hobbies": [
+        "coding",
+        "gaming",
+        "traveling"
+    ],          
+    "education": {
+        "degree": "Bachelor's in Computer Science",
+        "university": "Shivaji University",
+        "graduation_year": 2023
+    }
+}
+
+console.log(json);
+
+
